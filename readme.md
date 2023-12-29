393f35339f360f4b851b0033c49cae2de7fe8140
Nick Lundin <nick@moontower.net>
2023-12-28 19:36:00 -0600

    More layers. Readme

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Images
+
+### Links
+- [DockerHub Registry](https://hub.docker.com/repository/docker/nlundin08/pde/general)
+- [Dotfiles repo](https://github.com/nicklundin08-pde/lazyvim-dotfiles)
---
+++ ubuntu/002_lazyvim/scripts/install-lvim.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# https://dev.to/asyncedd/building-neovim-from-source-1794
+
+apt-get update && apt-get install -y curl zip unzip git ripgrep
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+install lazygit /usr/local/bin
+
+git clone https://github.com/LazyVim/starter /root/.config/nvim
