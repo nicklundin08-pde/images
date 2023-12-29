332a26444d074e43467fb03d8952b550eedfabc2
Nick Lundin <nick@moontower.net>
2023-12-29 09:57:15 -0600

    DRY publish script

--- core/ubuntu/publish.sh
+++ core/ubuntu/publish.sh
@@ -2,27 +2,44 @@
 set -e 
 
 DIR=$(pwd)
-echo $DIR
-
-# publish intermediate images
-cd $DIR/001_nvim/ && chmod +x publish.sh && ./publish.sh
-cd $DIR/002_lazyvim/ && chmod +x publish.sh && ./publish.sh
-cd $DIR/003_sdk/ && chmod +x publish.sh && ./publish.sh
-cd $DIR/004_dotfiles/ && chmod +x publish.sh && ./publish.sh
-
-
-## publish final image
 IMAGE="nlundin08/pde"
-TAG="ubuntu"
-IMAGE_TAG="$IMAGE:$TAG"
-FINAL_IMAGE_DIR="$DIR/004_dotfiles/"
 
-cd $DIR
 source ../../.env
 echo 'Executing docker login'
 echo $DOCKER_HUB_PASSWORD | docker login -u $DOCKER_HUB_USERNAME --password-stdin
 
-cd $FINAL_IMAGE_DIR
-docker build . -t $IMAGE_TAG
-docker tag $IMAGE_TAG $IMAGE_TAG
-docker push $IMAGE_TAG
+publish_image(){
+    TAG=$1
+    CONTEXT_DIR=$2
+    IMAGE_TAG="$IMAGE:$TAG"
+    # echo "    publish image called! tag:$TAG ctx:$CONTEXT_DIR"
+    # echo "cd $CONTEXT_DIR"
+    cd "$CONTEXT_DIR"
+    # echo $CONTEXT_DIR | cd
+    # pwd
+    docker build . -t $IMAGE_TAG
+    docker tag $IMAGE_TAG $IMAGE_TAG
+    docker push $IMAGE_TAG
+}
+
+# SKIPS=("ubuntu_nvim/" "")
+IFS='-'
+for d in */ ; do
+    DIRECTORY=$d
+    # echo "$d"
+    # if [[ $(echo ${SKIPS[@]} | fgrep -w $d) ]]
+    # then
+    #     echo "    Should skip"
+    # else
+    #     echo "    Should not skip"
+    # fi
+    read -ra elements <<< "$d"
+    TAG_TO_PUBLISH=${elements[1]}
+    SANITIZED_TAG=${TAG_TO_PUBLISH%/}
+    # echo "publish_image "$TAG_TO_PUBLISH" `$DIRECTORY`"
+    publish_image "$SANITIZED_TAG" "$DIR/$d"
+done
+
+
+
+
